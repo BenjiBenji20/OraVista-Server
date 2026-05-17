5c3adf53e7becb6ecf387f88bdf87bff41b8eca2
Azathoth <imperialbenjie393@gmail.com>
2026-05-18 04:58:25 +0800

    fix: supabase ssl=false for conn string

--- server.js
+++ server.js
@@ -46,7 +46,10 @@ app.use('/uploads', (req, res, next) => {
 // DATABASE CONNECTION (PostgreSQL / Supabase)
 // ---------------------------------------------------------
 const db = new Pool({
-    connectionString: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DATABASE}?sslmode=require`,
+    connectionString: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DATABASE}`,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect()
